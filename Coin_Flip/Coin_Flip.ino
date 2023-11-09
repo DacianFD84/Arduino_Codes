28a14ab186053d9d01f65ffec850a9650832a11a
Dacian Florin Dedu <dacian@florindedu.ro>
2023-11-09 19:24:24 +0200

    Coin_Flip_Led

--- Coin_Flip/Coin_Flip.ino
+++ Coin_Flip/Coin_Flip.ino
@@ -1,6 +1,11 @@
+const int headsLedPin = 2;  // Pin number for the Heads LED
+const int tailsLedPin = 3;  // Pin number for the Tails LED
+unsigned long lastFlipTime = 0;  // Variable to track the time of the last flip
+
 void setup() {
   Serial.begin(9600); // initialize serial communication for debugging
-  randomSeed(analogRead(A0)); // seed the random number generator with an analogRead
+  pinMode(headsLedPin, OUTPUT);  // Set the Heads LED pin as an output
+  pinMode(tailsLedPin, OUTPUT);  // Set the Tails LED pin as an output
 }
 
 void loop() {
@@ -9,18 +14,30 @@ void loop() {
 
     // Check if the entered command is "flip"
     if (command.equals("flip")) {
-      // simulate a coin flip (random number 0 or 1)
-      int coinResult = random(2);
+      // Check if it's been at least 3 seconds since the last flip
+      if (millis() - lastFlipTime >= 3000) {
+        // simulate a coin flip (random number 0 or 1)
+        int coinResult = analogRead(A0) & 1;  // Use the least significant bit for randomness
 
-      // print the coin flip result to the Serial Monitor
-      if (coinResult == 0) {
-        Serial.println("Heads");
-      } else {
-        Serial.println("Tails");
+        // print the coin flip result to the Serial Monitor
+        if (coinResult == 0) {
+          Serial.println("Heads");
+          digitalWrite(headsLedPin, HIGH);  // Turn on the Heads LED
+          digitalWrite(tailsLedPin, LOW);   // Turn off the Tails LED
+        } else {
+          Serial.println("Tails");
+          digitalWrite(headsLedPin, LOW);   // Turn off the Heads LED
+          digitalWrite(tailsLedPin, HIGH);  // Turn on the Tails LED
+        }
+
+        delay(3000); // Wait for 3 seconds
+
+        // Turn off both LEDs
+        digitalWrite(headsLedPin, LOW);
+        digitalWrite(tailsLedPin, LOW);
+
+        lastFlipTime = millis(); // Record the time of the flip
       }
     }
   }
-
-  // Uncomment the line below if you want to continue with the continuous coin flips
-  // delay(100); // wait for a short time before checking again
 }
