57d74f1b9c13c045abbbf0516cace49a3c7fefdf
Dacian Florin Dedu <dacian@florindedu.ro>
2023-11-09 11:20:44 +0200

    Lottery_Generator_V2

--- Lottery_Generator_6-49/Lottery_Generator_6-49.ino
+++ Lottery_Generator_6-49/Lottery_Generator_6-49.ino
@@ -1,19 +1,38 @@
 void setup() {
   Serial.begin(9600); // initialize serial communication for debugging
+
+  // Seed the random number generator with analog noise
+  randomSeed(analogRead(A0));
+
+  // Add a delay to allow the random seed to change
+  delay(1000);
 }
 
 void loop() {
-  // generate and print 6 random numbers between 1 and 49
-  for (int i = 0; i < 6; i++) {
-    int randomNumber = random(1, 50);
-    Serial.print(randomNumber);
-    
-    if (i < 5) {
-      Serial.print(", "); // print a comma and space between numbers
+  if (Serial.available() > 0) {
+    String input = Serial.readStringUntil('\n'); // read the input until a newline character is received
+
+    if (input == "generate") {
+      int generatedNumbers[6];
+
+      // generate 6 random numbers between 1 and 49
+      for (int i = 0; i < 6; i++) {
+        generatedNumbers[i] = random(1, 50);
+      }
+
+      // Print the generated numbers on one line
+      Serial.print("Loto 6/49 : ");
+      for (int i = 0; i < 6; i++) {
+        Serial.print(generatedNumbers[i]);
+
+        if (i < 5) {
+          Serial.print(", "); // print a comma and space between numbers
+        }
+      }
+
+      Serial.println(); // move to the next line for better readability
     }
   }
 
-  Serial.println(); // move to the next line for better readability
-
-  delay(1000); // wait for a second
+  // Your other code or delays can go here if needed
 }
