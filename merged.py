33b544cebd6221c4d9f9fe6890b0079edf9faaf3
Dacian Florin Dedu <dacian@florindedu.ro>
2023-11-14 12:11:04 +0200

    TEST

--- merged.py
+++ merged.py
@@ -1,34 +1,34 @@
 import subprocess
 
-def git_command(command)
+def git_command(command):
     subprocess.run(command, shell=True)
 
-def main()
+def main():
     # Initialize Git repository
-    git_command(git init)
+    git_command("git init")
 
     # Add all files to the staging area
-    git_command(git add .)
+    git_command("git add .")
 
     # Get user input for commit message
-    commit_message = input(Enter your commit message )
+    commit_message = input("Enter your commit message: ")
 
     # Commit changes
-    git_command(f'git commit -m {commit_message}')
+    git_command(f'git commit -m "{commit_message}"')
 
     # Get user input for branch name
-    branch_name = input(Enter the branch name (e.g., main, master) )
+    branch_name = input("Enter the branch name (e.g., main, master): ")
 
     # Set upstream and push to the specified branch
     git_command(f'git push -u origin {branch_name}')
 
     # Ask if the user wants to pull changes
-    pull_option = input(Do you want to pull changes from the repository (yn) )
+    pull_option = input("Do you want to pull changes from the repository? (y/n): ")
 
-    if pull_option.lower() == 'y'
+    if pull_option.lower() == 'y':
         # Fetch and merge changes
-        git_command(git fetch origin)
-        git_command(f'git merge origin{branch_name}')
+        git_command("git fetch origin")
+        git_command(f'git merge origin/{branch_name}')
 
-if __name__ == __main__
+if __name__ == "__main__":
     main()
